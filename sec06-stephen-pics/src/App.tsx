72f8d90f6b37a88513260f8d32d4efdcb045fceb
Deba Karan Agrawala <karan.deba@gmail.com>
2026-03-19 00:02:37 +0530

    feat: implement SearchBar with controlled input and callback integration
    - created SearchBar component with TypeScript props
    - added controlled input using useState
    - handled form submission with preventDefault
    - passed search term to parent via onSubmit callback
    - integrated SearchBar in App component

--- sec06-stephen-pics/src/App.tsx
+++ sec06-stephen-pics/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import SearchBar from './components/SearchBar'
 
 function App() {
 
+  //callback function
+  const handleSubmit = (term: string) => {
+    console.log(`Do a seach with ${term}`);
+  }
+
   return (
     <>
-
+      <SearchBar onSubmit={handleSubmit} />
     </>
   )
 }
--- sec06-stephen-pics/src/components/SearchBar.tsx
+++ sec06-stephen-pics/src/components/SearchBar.tsx
@@ -1,7 +1,37 @@
-const SearchBar = () => {
-    return (
-        <div>SearchBar</div>
-    )
+import type React from "react";
+import { useState } from "react";
+
+interface SearchBarProps {
+    onSubmit: (term: string) => void;
 }
 
-export default SearchBar
+const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
+    const [term, setTerm] = useState<string>("");
+
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (!term.trim()) return; // optional validation
+
+        onSubmit(term);
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTerm(event.target.value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleFormSubmit}>
+                <input
+                    type="text"
+                    value={term}
+                    onChange={handleChange}
+                    placeholder="Search images..."
+                />
+            </form>
+        </div>
+    );
+};
+
+export default SearchBar;
