3d6ac40e5ec18adcd91a2ba2aa078247d5d00a61
Deba Karan Agrawala <karan.deba@gmail.com>
2026-03-18 22:17:27 +0530

    refactor: centralize Unsplash API config using axios instance
    - created reusable axios instance with baseURL and headers
    - removed repeated config from individual requests
    - simplified API call using api.get()

--- sec06-stephen-pics/src/api.ts
+++ sec06-stephen-pics/src/api.ts
@@ -13,12 +13,16 @@ interface UnsplashResponse {
     results: UnsplashImage[];
 }
 
+const api = axios.create({
+    baseURL: "https://api.unsplash.com",
+    headers: {
+        Authorization: 'Client-ID WrOgk2HBj6MU49uwU9JZNXg4en2xmt4vq08TntSB0ak',
+    },
+})
+
 const searchImages = async (searchTerm: string): Promise<UnsplashImage[]> => {
     try {
-        const response = await axios.get<UnsplashResponse>('https://api.unsplash.com/search/photos', {
-            headers: {
-                Authorization: 'Client-ID WrOgk2HBj6MU49uwU9JZNXg4en2xmt4vq08TntSB0ak'
-            },
+        const response = await api.get<UnsplashResponse>("/search/photos", {
             params: {
                 query: searchTerm
             }
